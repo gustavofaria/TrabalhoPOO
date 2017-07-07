064d5a0b3cc75a74758e47381c5154e9b2602be8
dpememp <adrianoshirost@gmail.com>
2017-07-07 14:27:00 -0300

    Update Ship.java

--- BatalhaNaval/src/batalhanaval/Ship.java
+++ BatalhaNaval/src/batalhanaval/Ship.java
@@ -3,11 +3,19 @@ package batalhanaval;
 public class Ship {
     private int size;
     private int coord[];
+    private int hv;
     
-    //public Ship(int s,int[] c){
-    //    size = s;
-    //    coord = c;
-    //}
+    public Ship(int s,int c,int d){
+        size = s;
+        coord = new int[s];
+        coord[0] = c;
+        hv = d;
+    }
+    
+    public void addCoordinates(){
+        if(hv == 1) for(int i=0;i<size-1;i++) coord[i+1] = coord[i] + 1;
+        else for(int i=0;i<size-1;i++) coord[i+1] = coord[i] + 10;
+    }
     
     public boolean attacked(int x){
         for(int i=0;i<size;i++) if(coord[i] == x){
