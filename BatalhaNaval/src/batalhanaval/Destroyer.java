3421f585607ee0b68ed203dba7f957854aaf3d0c
dpememp <adrianoshirost@gmail.com>
2017-07-08 21:13:45 -0300

    Update Destroyer.java

--- BatalhaNaval/src/batalhanaval/Destroyer.java
+++ BatalhaNaval/src/batalhanaval/Destroyer.java
@@ -2,9 +2,7 @@ package batalhanaval;
 
 public class Destroyer extends Ship {
     
-    public Destroyer (int c,int d){
-        super(2,c,d);
-        super.addCoordinates();
+    public Destroyer (int c[]){
+        super(2,c);
     }
-    
 }
