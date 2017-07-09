ac63eac7cce1053c795e2053bb9ca64efc53645d
dpememp <adrianoshirost@gmail.com>
2017-07-09 20:48:00 -0300

    Update CPU.java

--- BatalhaNaval/src/batalhanaval/CPU.java
+++ BatalhaNaval/src/batalhanaval/CPU.java
@@ -4,19 +4,84 @@ import java.util.Random;
 public class CPU extends Player{
     private int attks[];
     private int index;
+    private int hit;
+    private int nb;
+    private int nc;
+    private int nd;
+    private int ns;
     
     public CPU(){
         super();
         attks = new int[100];
-        index = 0;
+        index = -1;
+        hit = 0;
+        nb = 1;
+        nc = 2;
+        nd = 3;
+        ns = 4;
+        while(nb != 0 ){
+            Random r = new Random();
+            int d = (r.nextInt(2)*2)-1;
+            int c = r.nextInt(100);
+            int vet[] = validPI(4,d,c);
+            if(vet != null){
+                BattleShip bs = new BattleShip(vet);
+                addShip(bs);
+                nb--;
+            }
+            
+        }
+        while(nc != 0 ){
+            Random r = new Random();
+            int d = (r.nextInt(2)*2)-1;
+            int c = r.nextInt(100);
+            int vet[] = validPI(3,d,c);
+            if(vet != null){
+                Cruiser cr = new Cruiser(vet);
+                addShip(cr);
+                nc--;
+            }
+            
+        }
+        while(nd != 0 ){
+            Random r = new Random();
+            int d = (r.nextInt(2)*2)-1;
+            int c = r.nextInt(100);
+            int vet[] = validPI(2,d,c);
+            if(vet != null){
+                Destroyer dr = new Destroyer(vet);
+                addShip(dr);
+                nd--;
+            }
+        
+        }
+        while(ns != 0 ){
+            Random r = new Random();
+            int d = (r.nextInt(2)*2)-1;
+            int c = r.nextInt(100);
+            int vet[] = validPI(1,d,c);
+            if(vet != null){
+                Submarine sb = new Submarine(vet);
+                addShip(sb);
+                ns--;
+            }
+        
+        }
     }
     
-    public int attacking(){
+    public int attacking(Player p1){
         Random rg = new Random();
-        int x = rg.nextInt(100);
-        while(!belongs(x)) x = rg.nextInt(100);
-        index++;
-        return x;
+        
+            int x = rg.nextInt(100);
+            while(belongs(x)) x = rg.nextInt(100);
+            index++;
+            attks[index] = x;
+            if(p1.getAttacked(x)){
+                hit = 1;
+                return (x+1);
+            }
+            else return (-1)*(x+1);
+
     }
     
     public boolean belongs(int x){
@@ -24,3 +89,4 @@ public class CPU extends Player{
         return false;
     }
 }
+
