c6a62d793227d906122a7b999072e5347008d839
dpememp <adrianoshirost@gmail.com>
2017-07-08 21:12:53 -0300

    Update BattleShip.java

--- BatalhaNaval/src/batalhanaval/BattleShip.java
+++ BatalhaNaval/src/batalhanaval/BattleShip.java
@@ -2,9 +2,8 @@ package batalhanaval;
 
 public class BattleShip extends Ship {
 
-    public BattleShip(int c,int d){
-        super(4,c,d);
-        super.addCoordinates();
+    public BattleShip(int c[]){
+        super(4,c);
     }
     
 
