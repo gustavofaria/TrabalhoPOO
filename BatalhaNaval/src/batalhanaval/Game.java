68bf52da03516c3f041edb76a2f9dbf70009d928
dpememp <adrianoshirost@gmail.com>
2017-07-11 20:31:07 -0300

    Update Game.java

--- BatalhaNaval/src/batalhanaval/Game.java
+++ BatalhaNaval/src/batalhanaval/Game.java
@@ -6,6 +6,27 @@ import javax.swing.*;
 public class Game {
     public Game (Player p1,CPU cpu){
         String numbers[] = new String[100];
+        Icon icsub = new ImageIcon(getClass().getResource("images/submarine.png"));
+        Icon ichdt1 = new ImageIcon(getClass().getResource("images/destroyer1h.jpg"));
+        Icon ichdt2 = new ImageIcon(getClass().getResource("images/destroyer2h.jpg"));
+        //Icon icvdt1 = new ImageIcon(getClass().getResource("images/destroyer1v.jpg"));
+        //Icon icvdt2 = new ImageIcon(getClass().getResource("images/destroyer2v.jpg"));
+        Icon ichcr1 = new ImageIcon(getClass().getResource("images/cruiser1h.jpg"));
+        Icon ichcr2 = new ImageIcon(getClass().getResource("images/cruiser2h.jpg"));
+        Icon ichcr3 = new ImageIcon(getClass().getResource("images/cruiser3h.jpg"));
+        //Icon icvcr1 = new ImageIcon(getClass().getResource("images/cruiser1v.jpg"));
+        //Icon icvcr2 = new ImageIcon(getClass().getResource("images/cruiser2v.jpg"));
+        //Icon icvcr3 = new ImageIcon(getClass().getResource("images/cruiser3v.jpg"));
+        //Icon ichbs1 = new ImageIcon(getClass().getResource("images/battleship1h.jpg"));
+        //Icon ichbs2 = new ImageIcon(getClass().getResource("images/battleship2h.jpg"));
+        //Icon ichbs3 = new ImageIcon(getClass().getResource("images/battleship3h.jpg"));
+        //Icon ichbs4 = new ImageIcon(getClass().getResource("images/battleship4h.jpg"));
+        //Icon icvbs1 = new ImageIcon(getClass().getResource("images/battleship1v.jpg"));
+        //Icon icvbs2 = new ImageIcon(getClass().getResource("images/battleship2v.jpg"));
+        //Icon icvbs3 = new ImageIcon(getClass().getResource("images/battleship3v.jpg"));
+        //Icon icvbs4 = new ImageIcon(getClass().getResource("images/battleship4v.jpg"));
+        
+        
         final JFrame janela = new JFrame("Batalha Naval");
         janela.setSize(500,500);
         JLabel player = new JLabel("Jogador");
@@ -28,16 +49,25 @@ public class Game {
             public void actionPerformed(ActionEvent e){
                 JButton click = (JButton)e.getSource();
                 
-                int i = Integer.parseInt(click.getText());
+                int i = Integer.parseInt(click.getName());
                 
                 if(cpu.getAttacked(i)) {
                     click.setEnabled(false);
-                    click.setText("X");
+                    int vet[];
+                    vet = p1.helpImages(i);
+                    if(vet[0] == 1) click.setIcon(icsub);
+                    if(vet[0] == 2){
+                        if(vet[1] == 1) click.setIcon(ichdt1);
+                        if(vet[1] == 2) click.setIcon(ichdt2);
+                    }
+                    if(vet[0] == 3){
+                        
+                    }
+                    if(vet[0] == 4){
+                        
+                    }
                 }
-                else{
-                    click.setEnabled(false);
-                    click.setText("O");
-                }
+                else click.setEnabled(false);
                 
                 if(cpu.endOfGame()) {
                     
@@ -50,8 +80,17 @@ public class Game {
                 if(p1.endOfGame()) JOptionPane.showMessageDialog(null,"VOCE PERDEU !!!");
             }
         };
+
+        Icon icone = new ImageIcon(getClass().getResource("images/sea.png"));
         
-        for(int i=0;i<100;i++)((JButton)cpushp.add(new JButton(numbers[i]))).addActionListener(getnumb);
+        for(int i=0;i<100;i++){
+            JButton dumb = new JButton(icone);
+            dumb.setName(Integer.toString(i));
+            dumb.addActionListener(getnumb);
+            cpushp.add(dumb);
+        }        
+        
+        
         right.add(plyshp,BorderLayout.CENTER);
         right.add(player,BorderLayout.NORTH);
         left.add(cpushp,BorderLayout.CENTER);
