7ac9a90f7cf6447c2bb2d93c662e810416ec3c3e
dpememp <adrianoshirost@gmail.com>
2017-07-15 09:33:36 -0300

    Update main.java

--- BatalhaNaval/src/batalhanaval/main.java
+++ BatalhaNaval/src/batalhanaval/main.java
@@ -1,11 +1,40 @@
 package batalhanaval;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 
 public class main {
-
+    
+    public main(){
+        
+        JFrame janela = new JFrame("Batalha Naval");
+        janela.setSize(500,500);
+        JPanel panel = new JPanel(new BorderLayout());
+        JButton pl = new JButton("Jogar");
+        JLabel image = new JLabel();
+        Icon war = new ImageIcon(getClass().getResource("images/mainimage3.jpg"));
+        
+        ActionListener numb = new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                janela.setVisible(false);
+                new setUP();
+            }
+        };
+        image.setIcon(war);
+        panel.add(image,BorderLayout.CENTER);
+        pl.addActionListener(numb);
+        panel.add(pl,BorderLayout.SOUTH);
+        janela.add(panel);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
     public static void main(String[] args) {
         
-        new setUP();
+        new main();
     }
 
 }
+
